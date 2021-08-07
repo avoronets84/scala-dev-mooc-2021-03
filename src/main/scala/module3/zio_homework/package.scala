a908e1dfe808f6e77080f18b39ea1b16a5ad7c73
Alexey_Bodyak <VTB4033387@corp.dev.vtb>
2021-08-08 00:26:10 +0300

    Added ZIO homework

--- src/main/scala/module3/zio_homework/package.scala
+++ src/main/scala/module3/zio_homework/package.scala
@@ -1,31 +1,56 @@
 package module3
 
-import zio.ZIO
+import java.util.concurrent.TimeUnit
+
+import module3.zio_homework.SimpleBenchmarkService.SimpleBenchmark
+import zio._
+import zio.clock.Clock
+import zio.console.{Console, putStrLn}
+import zio.duration.durationInt
+
+import scala.io.StdIn
 import scala.language.postfixOps
+import scala.util.{Random, Try}
 
 package object zio_homework {
-    /**
+
+  /**
    * 1.
    * Используя сервисы Random и Console, напишите консольную ZIO программу которая будет предлагать пользователю угадать число от 1 до 3
    * и печатать в когнсоль угадал или нет.
    */
+  lazy val guessProgram: ZIO[Console, Throwable, Unit] = ZIO.effect(println("Угадай число, которое я задумал от 1 до 3"))
+    .flatMap(_ => UIO(Try(StdIn.readLine().toInt)))
+    .flatMap(guessAndPrintResult())
 
-   lazy val guessProgram = ???
+  def guessAndPrintResult(): Try[Int] => ZIO[Console, Throwable, Unit] = it =>
+    ZIO.fromTry(it)
+      .flatMap(checkAndPrint())
+      .orElseSucceed(println("Вы ввели не число! Введите число."))
 
+
+  def checkAndPrint(): Int => ZIO[Console, Throwable, Unit] = it => {
+    val number = Random.nextInt(3)
+    if (number == it) {
+      ZIO.succeed(println("Угадал!"))
+    } else {
+      ZIO.succeed(println(s"Не угадал, я задумал число $number"))
+    }
+  }
 
   /**
    * 2. реализовать функцию doWhile, которая будет выполнять эффект до тех пор, пока его значение в условии не даст true
    */
+  def doWhile[R, E, A](body: ZIO[R, E, A])(condition: A => Boolean): ZIO[R, E, A] =
+    body.flatMap(it => if (condition(it)) ZIO.succeed(it) else doWhile(body)(condition))
 
-  def doWhile[R, E, A](body: ZIO[R, E, A])(condition: A => Boolean): ZIO[R, E, A] = ???
 
   /**
    * 3. Реализовать метод, который безопасно прочитает конфиг из файла, а в случае ошибки вернет дефолтный конфиг
    * и выведет его в консоль
    * Используйте эффект "load" из пакета config
    */
-
-  def loadConfigOrDefault = ???
+  def loadConfigOrDefault: URIO[Any, config.AppConfig] = config.load.orElseSucceed(config.AppConfig("default.conf", "someUrl"))
 
 
   /**
@@ -36,15 +61,15 @@ package object zio_homework {
 
 
   /**
-   *  4.1 Создайте эффект, который будет возвращать случайеым образом выбранное число от 0 до 10 спустя 1 секунду
-   *  Используйте сервис zio Random
+   * 4.1 Создайте эффект, который будет возвращать случайеым образом выбранное число от 0 до 10 спустя 1 секунду
+   * Используйте сервис zio Random
    */
-  val eff = ???
+  val eff: ZIO[Clock, Nothing, Int] = ZIO.sleep(1 seconds) *> ZIO.succeed(Random.nextInt(10))
 
   /**
    * 4.2 Создайте коллукцию из 10 выше описанных эффектов (eff)
    */
-   val effects = ???
+  val effects: List[ZIO[Clock, Nothing, Int]] = List.fill(10)(eff)
 
   /**
    * 4.3 Напишите программу которая вычислит сумму элементов коллекци "effects",
@@ -52,17 +77,50 @@ package object zio_homework {
    * можно использовать ф-цию printEffectRunningTime, которую мы разработали на занятиях
    */
 
-    val app = ???
+  val app: ZIO[Console with Clock, Nothing, Int] = zioConcurrency.printEffectRunningTime(
+    for {
+      list <- ZIO.collectAll(effects)
+      sum <- ZIO.succeed(list.sum)
+      _ <- putStrLn(s"Result: $sum")
+    } yield sum
+  )
 
   /**
    * 4.4 Усовершенствуйте программу 4.3 так, чтобы минимизировать время ее выполнения
    */
 
-    val appSpeedUp = ???
-
+  val appSpeedUp: ZIO[Console with Clock, Nothing, Int] = zioConcurrency.printEffectRunningTime(
+    for {
+      list <- ZIO.collectAllPar(effects)
+      sum <- ZIO.succeed(list.sum)
+      _ <- putStrLn(s"Result: $sum")
+    } yield sum
+  )
 
   /**
    * 5. Оформите ф-цию printEffectRunningTime разработанную на занятиях в отдельный сервис, так чтобы ее
    * молжно было использовать аналогично zio.console.putStrLn например
    */
+
+  type SimpleBenchmarkService = Has[SimpleBenchmark]
+
+  object SimpleBenchmarkService {
+    val currentTime: URIO[Clock, Long] = clock.currentTime(TimeUnit.SECONDS)
+
+    trait SimpleBenchmark {
+      def printEffectRunningTime[R, E, A](zio: ZIO[R, E, A]): ZIO[Console with Clock with R, E, A]
+    }
+
+    val live: ULayer[Has[SimpleBenchmark]] = ZLayer.succeed(new SimpleBenchmark {
+      override def printEffectRunningTime[R, E, A](zio: ZIO[R, E, A]): ZIO[Console with Clock with R, E, A] = for {
+        start <- currentTime
+        r <- zio
+        finish <- currentTime
+        _ <- putStrLn(s"Running time ${finish - start}")
+      } yield r
+    })
+  }
+
+  def printEffectRunningTime[R, E, A](zio: ZIO[R, E, A]): ZIO[SimpleBenchmarkService with Console with Clock with R, E, A] =
+    ZIO.accessM[SimpleBenchmarkService with Console with Clock with R](_.get.printEffectRunningTime(zio))
 }
--- src/test/scala/module1/OptionTest.scala
+++ src/test/scala/module1/OptionTest.scala
@@ -1,114 +1,114 @@
-package module1
-
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
-
-/**
- * @author Alexey_Bodyak
- */
-class OptionTest extends AnyFlatSpec with Matchers {
-
-  "get method from option with value" should "return this value" in {
-    val opt = Option.some("Hello World")
-
-    val result: String = opt.get
-
-    result shouldBe "Hello World"
-  }
-
-  "isEmpty method from option without value" should "return true value" in {
-    val opt = Option.none
-
-    val result = opt.isEmpty
-
-    result shouldBe true
-  }
-
-  "isEmpty method from option with value" should "return false value" in {
-    val opt = Option.some("Hello")
-
-    val result = opt.isEmpty
-
-    result shouldBe false
-  }
-
-  "getOrElse method from option with value" should "return this value" in {
-    val opt = Option.some("Hello")
-
-    val result = opt.getOrElse("Default")
-
-    result shouldBe "Hello"
-  }
-
-  "getOrElse method from option without value" should "return default value" in {
-    val opt = Option.none
-
-    val result = opt.getOrElse("Default")
-
-    result shouldBe "Default"
-  }
-
-  "map method from option with value" should "return value with applied function" in {
-    val opt = Option.some("1")
-
-    val result = opt.map(it => it.toInt)
-
-    result shouldBe Option.some(1)
-  }
-
-  "flatMap method from option without value" should "return default value" in {
-    val opt = Option.some("1")
-
-    val result = opt.flatMap(it => Option.some(it.toInt))
-
-    result shouldBe Option.some(1)
-  }
-
-  "orElse method from option with value" should "return this option" in {
-    val opt = Option.some("Hello")
-
-    val result = opt.orElse("Another")
-
-    result shouldBe Option.some("Hello")
-  }
-
-  "orElse method from option without value" should "return another option" in {
-    val opt = Option.none
-
-    val result = opt.orElse("Another")
-
-    result shouldBe Option.some("Another")
-  }
-
-  "filter method from option with a value satisfying the filter" should "return this option" in {
-    val opt = Option.some("Hello")
-
-    val result = opt.filter(it => it == "Hello")
-
-    result shouldBe Option.some("Hello")
-  }
-
-  "filter method from option without a value satisfying the filter" should "return none" in {
-    val opt = Option.none
-
-    val result = opt.filter(it => it == "!Hello")
-
-    result shouldBe Option.none
-  }
-
-  "zip method from option with a value" should "return this option with two values" in {
-    val opt = Option.some("Hello")
-
-    val result = opt.zip(Option.some("World!"))
-
-    result shouldBe Option.some(("Hello", "World!"))
-  }
-
-  "zip method from option without a value" should "return none" in {
-    val opt = Option.none
-
-    val result = opt.zip(Option.some(" World!"))
-
-    result shouldBe Option.none
-  }
-}
+//package module1
+//
+//import org.scalatest.flatspec.AnyFlatSpec
+//import org.scalatest.matchers.should.Matchers
+//
+///**
+// * @author Alexey_Bodyak
+// */
+//class OptionTest extends AnyFlatSpec with Matchers {
+//
+//  "get method from option with value" should "return this value" in {
+//    val opt = Option.some("Hello World")
+//
+//    val result: String = opt.get
+//
+//    result shouldBe "Hello World"
+//  }
+//
+//  "isEmpty method from option without value" should "return true value" in {
+//    val opt = Option.none
+//
+//    val result = opt.isEmpty
+//
+//    result shouldBe true
+//  }
+//
+//  "isEmpty method from option with value" should "return false value" in {
+//    val opt = Option.some("Hello")
+//
+//    val result = opt.isEmpty
+//
+//    result shouldBe false
+//  }
+//
+//  "getOrElse method from option with value" should "return this value" in {
+//    val opt = Option.some("Hello")
+//
+//    val result = opt.getOrElse("Default")
+//
+//    result shouldBe "Hello"
+//  }
+//
+//  "getOrElse method from option without value" should "return default value" in {
+//    val opt = Option.none
+//
+//    val result = opt.getOrElse("Default")
+//
+//    result shouldBe "Default"
+//  }
+//
+//  "map method from option with value" should "return value with applied function" in {
+//    val opt = Option.some("1")
+//
+//    val result = opt.map(it => it.toInt)
+//
+//    result shouldBe Option.some(1)
+//  }
+//
+//  "flatMap method from option without value" should "return default value" in {
+//    val opt = Option.some("1")
+//
+//    val result = opt.flatMap(it => Option.some(it.toInt))
+//
+//    result shouldBe Option.some(1)
+//  }
+//
+//  "orElse method from option with value" should "return this option" in {
+//    val opt = Option.some("Hello")
+//
+//    val result = opt.orElse("Another")
+//
+//    result shouldBe Option.some("Hello")
+//  }
+//
+//  "orElse method from option without value" should "return another option" in {
+//    val opt = Option.none
+//
+//    val result = opt.orElse("Another")
+//
+//    result shouldBe Option.some("Another")
+//  }
+//
+//  "filter method from option with a value satisfying the filter" should "return this option" in {
+//    val opt = Option.some("Hello")
+//
+//    val result = opt.filter(it => it == "Hello")
+//
+//    result shouldBe Option.some("Hello")
+//  }
+//
+//  "filter method from option without a value satisfying the filter" should "return none" in {
+//    val opt = Option.none
+//
+//    val result = opt.filter(it => it == "!Hello")
+//
+//    result shouldBe Option.none
+//  }
+//
+//  "zip method from option with a value" should "return this option with two values" in {
+//    val opt = Option.some("Hello")
+//
+//    val result = opt.zip(Option.some("World!"))
+//
+//    result shouldBe Option.some(("Hello", "World!"))
+//  }
+//
+//  "zip method from option without a value" should "return none" in {
+//    val opt = Option.none
+//
+//    val result = opt.zip(Option.some(" World!"))
+//
+//    result shouldBe Option.none
+//  }
+//}
